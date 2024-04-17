676d798221fe3fba1d41b7029f03ee1cebbd43e8
carolinarodrigues21 <carolina.niklaus.moreira.da.rocha.rodrigues@cern.ch>
2024-04-17 22:51:21 +0200

    Improve variable names

--- main.py
+++ main.py
@@ -12,37 +12,39 @@ from sandpile import pilhaDeAreia
 L = 100               
 
 #número total de grãos colocados no sistema
-graoFinal = 10000
+totalDeGraos = 10000
 
-#valores da constante p
-valoresDeP = [0.01, 0.1, 0.2, 0.5, 0.8, 0.9]
-legenda = []
-formatinho = ["o","p","v","*","^","s"]
+#valores da constante de probabilidade (p)
+probabilidades = [0.01, 0.1, 0.2, 0.5, 0.8, 0.9]
 
-for i in range(len(valoresDeP)):
-    listaAvalanche = pilhaDeAreia(L, graoFinal, valoresDeP[i])
-    legenda.append("p = " + str(valoresDeP[i]))
+#informações para o gráfico
+legendaGrafico = []
+formatoScatter = ["o","p","v","*","^","s"]
 
-    maiorzao = np.max(listaAvalanche)
-    valoresDeAvalanche = range(0,int(maiorzao)+1)
+for i in range(len(probabilidades)):
+    listaAvalanche = pilhaDeAreia(L, totalDeGraos, probabilidades[i])
+    legendaGrafico.append("p = " + str(probabilidades[i]))
 
-    frequencias = []
+    maiorAvalanche = np.max(listaAvalanche)
+    valoresDeAvalanche = range(0,int(maiorAvalanche)+1)
+
+    frequenciasDeAvalanches = []
     contador = 0
 
     for c in valoresDeAvalanche:
         for j in range(len(listaAvalanche)):
             if listaAvalanche[j] == c:
                 contador += 1
-        frequencias.append(contador)
+        frequenciasDeAvalanches.append(contador)
         contador = 0
 
-    plt.scatter(valoresDeAvalanche,frequencias, marker=formatinho[i])
+    plt.scatter(valoresDeAvalanche,frequenciasDeAvalanches, marker=formatoScatter[i])
 
 plt.xscale('log')
 plt.yscale('log')
-plt.legend(legenda)
+plt.legend(legendaGrafico)
 plt.xlim((0.8,500))
-plt.ylim((0.8,np.max(np.array(frequencias))+1000))
+plt.ylim((0.8,np.max(np.array(frequenciasDeAvalanches))+1000))
 plt.xlabel("Tamanho da Avalanche (Energia)")
 plt.ylabel("Frequência Normalizada")
 plt.show()
