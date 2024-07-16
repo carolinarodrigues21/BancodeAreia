7e2c3d1bc12def1b62cbeb205c83a2cb7903010e
luccatfqcmartins <luccatfqcmartins@users.noreply.github.com>
2024-07-16 14:17:43 -0300

    Scatter histogram

--- histograms.py
+++ histograms.py
@@ -6,8 +6,14 @@ def normalizedAvalanchesHistogram(data, p):
     for n in range(16):
         limites.append(1.4**n)
 
-    plt.hist(data, bins=limites, edgecolor='black', density=True)
+    n, bins = np.histogram(data, bins=limites)
+
+    bins_mean = [0.5 * (bins[i] + bins[i+1]) for i in range(len(n))]
+    plt.scatter(bins_mean, n)
     plt.xscale('log')
+    plt.yscale('log')
+    plt.xlim([1,1000])
+    plt.ylim([1,1000])
     plt.title('Histogram for %.2f' %(p))
     plt.xlabel('Value')
     plt.ylabel('Frequency')
--- main.py
+++ main.py
@@ -16,11 +16,13 @@ L = 100
 totalDeGraos = 10000
 
 #valores da constante de probabilidade (p)
-probabilidades = [0.01, 0.1, 0.2, 0.5, 0.8, 0.9]
+# probabilidades = [0.01, 0.1, 0.2, 0.5, 0.8, 0.9]
+probabilidades = [0.01]
 
 #informações para o gráfico
 legendaGrafico = []
-formatoScatter = ["o","p","v","*","^","s"]
+# formatoScatter = ["o","p","v","*","^","s"]
+formatoScatter = ["o"]
 
 for i in range(len(probabilidades)):
     listaAvalanche = pilhaDeAreia(L, totalDeGraos, probabilidades[i])
