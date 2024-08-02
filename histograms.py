5f5d10b2bf07d3f9a03404e6b185446d4d074515
luccatfqcmartins <luccatfqcmartins@users.noreply.github.com>
2024-08-02 13:39:27 -0300

    Co-authored-by: Carolina Niklaus <carolinarodrigues21@users.noreply.github.com>

--- histograms.py
+++ histograms.py
@@ -10,6 +10,13 @@ def normalizedAvalanchesHistogram(data, p):
     n, bins = np.histogram(data, bins=limites)
 
     bins_mean = [0.5 * (bins[i] + bins[i+1]) for i in range(len(n))]
+    n_norm = [n[i]/(np.sum(data)*(1.5**(i+1) - 1.5**(i))) for i in range(len(n))]
+    plt.scatter(bins_mean, n_norm)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlim([1,1000])
+    plt.ylim([0,1])
+    plt.show()
     plt.scatter(bins_mean, n)
     plt.xscale('log')
     plt.yscale('log')
